82292eb05f718fac5cfec82dd6c641d208cca247
Miroslav Stefanović <mstef@uns.ac.rs>
2021-04-07 23:45:40 +0200

    Vežbe 08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VoziloComponent } from './vozilo/vozilo.component';
+import { AutomobilComponent } from './vozilo/automobil/automobil.component';
+import { ArtiklComponent } from './artikl/artikl.component';
+import { DobavljacComponent } from './dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './stavka-porudzbine/stavka-porudzbine.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { AuthorComponent } from './core/author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoziloComponent,
+    AutomobilComponent,
+    ArtiklComponent,
+    DobavljacComponent,
+    PorudzbinaComponent,
+    StavkaPorudzbineComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/porudzbina/porudzbina.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PorudzbinaComponent } from './porudzbina.component';
+
+describe('PorudzbinaComponent', () => {
+  let component: PorudzbinaComponent;
+  let fixture: ComponentFixture<PorudzbinaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PorudzbinaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PorudzbinaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
